b37b954e4020db2c69c84c4a948d448cdf161946
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 18:26:20 +0530

    Making GitButton component

---
+++ src/components/GitButton.js
@@ -0,0 +1,7 @@
+export default function GitButton() {
+  return (
+    <a href="https://github.com/NanotechPikachu/anichu.db/" target="_blank" rel="noopener noreferrer">
+    <button className="bg-transparent text-black mt-10 rounded-full text-center py-2 px-4">GitHub repository</button>
+    </a>
+  )
+}
