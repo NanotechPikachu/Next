e4a43afa627863cc1458801687d216ccd202eda3
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 18:53:27 +0530

    Update components

---
+++ src/components/CodeBlock.js
@@ -0,0 +1,5 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const style = a11yDark;
+export { SyntaxHighlighter, style };
