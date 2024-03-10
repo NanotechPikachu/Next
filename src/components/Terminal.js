ae49485432f2a98357a0fdaa2bfed9c67b83ac52
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 19:46:35 +0530

    Update components

---
+++ src/components/Terminal.js
@@ -0,0 +1,24 @@
+import { ClipboardIcon } from '@heroicons/react/24/outline';
+
+
+export default function Terminal({ command }) {
+  return (
+    <>
+    <div className="flex justify-end items-center bg-black mt-4 ml-5 h-7 mr-5 md:w-96">
+    <button
+          onClick={() => handleCopy(command)}
+          className="text-gray-400 rounded p-1 mr-3 border hover:bg-zinc-950"
+        >
+    <ClipboardIcon className="h-4 w-4" />
+    </button>
+    </div>
+    <div className="flex justify-between items-center bg-zinc-900 text-green-400 p-3 ml-5 mr-5 md:w-96">
+    <span className="text-blue-500">$ {command}</span>
+    </div>
+    </>
+  )
+}
+
+function handleCopy(command) {
+  navigator.clipboard.writeText(command);
+};
