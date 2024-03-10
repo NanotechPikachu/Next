3d662b6c69d17834dc9d452e87d67c647154bee0
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-10 18:39:37 +0530

    make Code component

---
+++ src/components/Code.js
@@ -0,0 +1,7 @@
+export default function Code({ code, className }) {
+  return (
+    <span className={`backdrop-blur text-black bg-zinc-300/50 p-0.5 font-mono ${className}`}>
+    {code}
+    </span>
+  )
+}
