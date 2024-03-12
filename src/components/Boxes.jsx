101d8baa3804f00663c108e48f539830db89b9f4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-12 18:33:46 +0530

    Update Boxes.jsx

--- src/components/Boxes.jsx
+++ src/components/Boxes.jsx
@@ -5,7 +5,7 @@ export function Note({ title="Note", children }) {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
     </svg>
-    <p className="ml-4">{title}</p>
+    <p className="ml-4 text-neutral-400/50">{title}</p>
     </div>
     <p>{children}</p> 
     </div>
