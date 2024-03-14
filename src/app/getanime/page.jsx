d6c3a76a93d6a0c71ce28f76c9980e5ff8775af4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-14 18:32:50 +0530

    getAnime

---
+++ src/app/getanime/page.jsx
@@ -0,0 +1,19 @@
+import Code from '../../components/Code.js';
+import { NextCard, PrevCard } from '../../components/PageCards.js';
+import { SyntaxHighlighter as CodeBlock, style } from '../../components/CodeBlock.js';
+import Copy from '../../components/Copy.js';
+import TitleNavBar from '../../components/TitleNavBar.js';
+import { Note } from '../../components/Boxes.jsx';
+
+export default function GetAnime() {
+
+  return (
+    <div>
+    <h1 className="ml-4 text-3xl md:text-4xl pt-24 mb-4 font-bold text-black">get Anime()</h1>
+    <hr />
+    <p className="ml-5 mr-5 mt-4 text-black">
+    This page deals with the JSON DB output as well as discussing the results or outputs of functions.
+    </p>
+    </div>
+  )
+}
