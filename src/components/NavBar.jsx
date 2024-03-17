88cf36f802332758724d94677018851a11e2cf4e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-17 18:18:18 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,12 +12,17 @@ export default function NavBar() {
 
   return (
     <div>
+    <div className="backdrop-blur fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-10">
+    <p className="font-bold text-left text-sky-600 hover:text-amber-700 text-xl font-sans">
+    Nanotech Wiki
+    </p>
       <button
         className={`fixed z-30 top-0 right-0 m-4 p-2 border-2 ${isOpen ? 'hidden' : 'block'}`}
         onClick={toggleNavBar}
       >
         Menu
       </button>
+    </div>
       <div
         className={`fixed top-0 right-0 w-1/2 h-full bg-gray-100 p-4 transform ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
@@ -31,7 +36,7 @@ export default function NavBar() {
         </button>
         <nav>
           <ul>
-            <li className="mt-8">
+            <li className="mt-12">
               <Link href="/">
                 <a onClick={toggleNavBar}>Home</a>
               </Link>
