cb10ceff9a59f47a7720f48427dca09b884a9910
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-17 18:02:50 +0530

    Update components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleNavBar() {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button
+        className={`fixed z-30 top-6 right-0 m-4 p-2 border-2 ${isOpen ? 'hidden' : 'block'}`}
+        onClick={toggleNavBar}
+      >
+        Menu
+      </button>
+      <div
+        className={`fixed top-0 right-0 w-1/2 h-full bg-gray-100 p-4 transform ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        } transition-transform duration-500 ease-in-out md:w-1/3`}
+      >
+        <button
+          className="absolute top-0 left-0 m-4 p-2 border-2"
+          onClick={toggleNavBar}
+        >
+          Close
+        </button>
+        <nav>
+          <ul>
+            <li className="my-4">
+              <Link href="/">
+                <a onClick={toggleNavBar}>Home</a>
+              </Link>
+            </li>
+            {/* Add more links here */}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
