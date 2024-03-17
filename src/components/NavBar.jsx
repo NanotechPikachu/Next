7c21c20aa99f9c4ed052e157d0b5981f140aaea5
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-03-17 18:32:33 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,12 +37,20 @@ export default function NavBar() {
           isOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-500 ease-in-out md:w-1/3`}
       >
+<div className="absolute top-0 left-0 m-3 p-1 border-2"
+          onClick={toggleNavBar}>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
+</svg>
+</div>
+{/*
         <button
           className="absolute top-0 left-0 m-4 p-2 border-2"
           onClick={toggleNavBar}
         >
           Close
         </button>
+*/}
         <nav>
           <ul>
             <li className="mt-12">
