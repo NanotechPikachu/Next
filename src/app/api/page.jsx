a50e98fd77102843c799d0cff01111bc4fa2ae84
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-05-18 06:35:05 +0530

    Update app

---
+++ src/app/api/page.jsx
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+export default function UserProfile() {
+  const [ user, setUser ] = useState(null);
+
+
+  useEffect(() => {
+    const checkAuth = async () => {
+    try {
+      const log = await axios.get(`https://dash-six-beryl.vercel.app/auth/check`, { withCredentials: true }); console.log(log);
+  //    if (log?.data?.loggedIn) {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/user`, { withCredentials: true }); console.log(response);
+        setUser(response?.data);
+    //  } 
+    } catch (err) {
+      console.error(err);
+    }
+    };
+    checkAuth();
+  }, []);
+  return (
+    <>
+    {user ? JSON.stringify(user) : "loading"}
+    </>
+  )
+}
