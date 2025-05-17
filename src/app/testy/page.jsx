4413bbf5d0dfadfe6c9be45ad0248b6b273ca200
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2025-05-17 16:44:44 +0530

    Update page.jsx

--- src/app/testy/page.jsx
+++ src/app/testy/page.jsx
@@ -1,6 +1,6 @@
 export default function VideoPlay() {
 const url = 'https://vault-12.kwik.cx/mp4/12/09/949d262c0a4c08f7420ec823a4fc2d8f174823aafed036ca9dd378774b56c7b2?file=AnimePahe_Solo_Leveling_-_01_360p_SubsPlease.mp4';
 return (
-<video src={url} width={100} height={70} />
+<video src={url} controls width={200} height={100} />
 )
 }
