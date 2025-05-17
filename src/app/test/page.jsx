bd9a5e81a1c1837026c5862aad65b22b81dd04da
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2025-05-17 16:40:41 +0530

    Update page.jsx

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -8,7 +8,7 @@ import {
   defaultLayoutIcons,
   DefaultVideoLayout,
 } from "@vidstack/react/player/layouts/default";
-
+/*
 export default function VideoPlayer() {
   const [m3u8Url, setM3u8Url] = useState(""); // State to store the input URL
   const [proxyUrl, setProxyUrl] = useState(""); // State to store the proxy URL for playback
@@ -65,3 +65,10 @@ export default function VideoPlayer() {
     </div>
   );
 }
+*/
+
+export default function VideoPlay() {
+const url = 'https://vault-12.kwik.cx/mp4/12/09/949d262c0a4c08f7420ec823a4fc2d8f174823aafed036ca9dd378774b56c7b2?file=AnimePahe_Solo_Leveling_-_01_360p_SubsPlease.mp4';
+return (
+<video src={url} width={100} height={70} />
+)
